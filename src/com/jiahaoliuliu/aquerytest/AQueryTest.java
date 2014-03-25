1e23b1c1bef50aa1f07d80558993d25c89de52db
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-03-25 17:23:14 +0100

    Made some new lines

--- src/com/jiahaoliuliu/aquerytest/AQueryTest.java
+++ src/com/jiahaoliuliu/aquerytest/AQueryTest.java
@@ -11,5 +11,7 @@ public class AQueryTest extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        
     }
 }
