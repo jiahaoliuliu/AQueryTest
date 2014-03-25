d8d5e40231a79f3d1447dbc7c737f3f3686ac70c
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-03-25 17:39:52 +0100

    Created simple ajax example

--- src/com/jiahaoliuliu/aquerytest/AQueryTest.java
+++ src/com/jiahaoliuliu/aquerytest/AQueryTest.java
@@ -2,8 +2,17 @@ package com.jiahaoliuliu.aquerytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
+import org.json.JSONObject;
 
 public class AQueryTest extends Activity {
+
+    private TextView textView;
+
     /**
      * Called when the activity is first created.
      */
@@ -12,6 +21,24 @@ public class AQueryTest extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        textView = (TextView)findViewById(R.id.textView);
 
+        String url = "http://www.google.com/uds/GnewsSearch?q=Obama&v=1.0";
+        final AQuery aq = new AQuery(this);
+
+        aq.ajax(url, JSONObject.class, new AjaxCallback<JSONObject>() {
+
+                    @Override
+                    public void callback(String url, JSONObject json, AjaxStatus status) {
+                        if (json != null) {
+                            // Successful ajax call, show status code and json content
+                            textView.setText(status.getCode() + ":" + json.toString());
+                        } else {
+                            // ajax error, show error code
+                            textView.setText("Error:" + status.getCode());
+                        }
+                    }
+            }
+        );
     }
 }
